79b1ebb21a60a394f52ef60ecc1ec45fe82d870d
Rafa de la Hoz <rafa.delahoz@gmail.com>
2016-02-16 10:14:09 +0100

    Restored the Arcade settings to playable values.

--- source/Meta/ArcadeGameStatus.hx
+++ source/Meta/ArcadeGameStatus.hx
@@ -13,11 +13,11 @@ class ArcadeGameStatus
             arcadeGameData.mode = puzzle.PuzzleData.ModeEndless;
             arcadeGameData.background = null;
             arcadeGameData.bubbleSet = null;
-            arcadeGameData.initialRows = 1;
+            arcadeGameData.initialRows = 4;
             arcadeGameData.dropDelay = 20;
             arcadeGameData.seconds = -1;
             arcadeGameData.usedColors = [];
-            for (i in 0...1)
+            for (i in 0...5)
             {
                 arcadeGameData.usedColors.push(new BubbleColor(i));
             }
