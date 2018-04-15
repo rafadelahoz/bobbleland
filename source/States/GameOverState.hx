a526501489426f4dd2873c775a53a86391566f3c
Rafa de la Hoz <rafa.delahoz@gmail.com>
2018-04-15 11:22:14 +0200

    Started drafting game over ticket screen

--- source/States/GameOverState.hx
+++ source/States/GameOverState.hx
@@ -3,7 +3,11 @@ package;
 import flixel.FlxG;
 import flixel.FlxState;
 import flixel.FlxSprite;
+import flixel.group.FlxGroup;
+import flixel.group.FlxSpriteGroup;
 import flixel.text.FlxBitmapText;
+import flixel.tweens.FlxTween;
+import flixel.tweens.FlxEase;
 import flixel.addons.display.FlxBackdrop;
 import flixel.addons.transition.FlxTransitionableState;
 
@@ -12,11 +16,16 @@ import text.TextUtils;
 
 class GameOverState extends FlxTransitionableState
 {
-	public var labelLabel : FlxBitmapText;
-	public var scoreLabel : FlxBitmapText;
-
+	var buttonLayer : FlxGroup;
 	public var btnRetry : Button;
 	public var btnGiveup : Button;
+
+	var machineBG : FlxSprite;
+
+	var ticketLayer : FlxGroup;
+
+	var machineFG : FlxSprite;
+	var btnCheckout : FlxSprite;
 
 	var data : Dynamic;
 
@@ -37,47 +46,50 @@ class GameOverState extends FlxTransitionableState
 		var background : FlxBackdrop = database.BackgroundDatabase.BuildRandomBackground();
         background.scrollFactor.set(0.35, 0.35);
         background.velocity.set(0, -5);
-        add(background);
+        // add(background);
 
-		var backLayer : FlxSprite = new FlxSprite(0, 0, "assets/ui/gameover-bg.png");
-		add(backLayer);
+		var blackStrip : FlxSprite;
+		blackStrip = new FlxSprite(0, 272);
+		blackStrip.makeGraphic(FlxG.width, 48, 0xFF000000);
+		add(blackStrip);
 
-	/*	if (mode == PlayState.ModeArcade)
-		{
-			labelLabel = PixelText.New(16, 152, "Score:");
-			scoreLabel = PixelText.New(FlxG.width / 2, 152, TextUtils.padWith("" + data.score, 8, " "));
-			add(labelLabel);
-			add(scoreLabel);
-		}*/
+		// Build action button layer
+			buttonLayer = new FlxGroup();
+			add(buttonLayer);
 
-		var baseY : Int = 173;
+			btnGiveup = new Button(8, 296, onGiveupButtonPressed);
+			btnGiveup.loadSpritesheet("assets/ui/btn-gameover-tomenu.png", 80, 26);
+			buttonLayer.add(btnGiveup);
 
-		/*add(PixelText.New(16, baseY, "Play Time:"));
-		add(PixelText.New(FlxG.width / 2, baseY,
-						TextUtils.padWith(TextUtils.formatTime(data.playTime), 9)));
+			btnRetry = new Button(92, 296, onRetryButtonPressed);
+			btnRetry.loadSpritesheet("assets/ui/btn-gameover-again.png", 80, 26);
+			buttonLayer.add(btnRetry);
 
-		add(PixelText.New(16, baseY + 8, "Bubbles:"));
-		add(PixelText.New(FlxG.width / 2, baseY + 8,
-						TextUtils.padWith("" + data.bubbles, 8)));
+		// Build machine
+			machineBG = new FlxSprite(0, 264 + 64, "assets/ui/go-machine-bg.png");
+			add(machineBG);
 
-		add(PixelText.New(16, baseY + 16, "Cleans:"));
-		add(PixelText.New(FlxG.width / 2, baseY + 16,
-						TextUtils.padWith("" + data.cleans, 8)));*/
+			// Ticket will go here
+			ticketLayer = new FlxGroup();
+			add(ticketLayer);
 
-		btnGiveup = new Button(8, 256, onGiveupButtonPressed);
-		btnGiveup.loadSpritesheet("assets/ui/btn-gameover-tomenu.png", 80, 26);
-		add(btnGiveup);
+			machineFG = new FlxSprite(0, 264 + 64, "assets/ui/go-machine-fg.png");
+			add(machineFG);
 
-		btnRetry = new Button(92, 256, onRetryButtonPressed);
-		btnRetry.loadSpritesheet("assets/ui/btn-gameover-again.png", 80, 26);
-		add(btnRetry);
+			// TODO: Checkout button
 
-		new flixel.util.FlxTimer().start(0.25, function(t:flixel.util.FlxTimer) {
+		var appearDuration : Float = 0.5;
+		FlxTween.tween(machineBG, {y : 264}, appearDuration, {ease: FlxEase.circOut});
+		FlxTween.tween(machineFG, {y : 264}, appearDuration, {ease: FlxEase.circOut});
+		// TODO: Tween ticket button also!
+		
+		// Add ticket
+		// TODO: Make it print!
+		/*new flixel.util.FlxTimer().start(0.25, function(t:flixel.util.FlxTimer) {
 			var t : Ticket = new Ticket();
 			t.init(data);
-			add(t);
-			// BubbleShare.share("I just SOAP ALLEYed " + score + " points in " + TextUtils.formatTime(playTime) + "\". Yay!", true);
-		});
+			ticketLayer.add(t);
+		});*/
 	}
 
 	override public function update(elapsed:Float)
